1c715286960cc5306e97c78ccf11076884b35d08
Matheus <matheusalexandre.sena99@gmail.com>
2020-10-26 14:22:05 -0300

    Adicionando Swagger

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const cors = require('cors')
 const compression = require('compression')
 const http = require('http');
 const https = require('https');
-
 const dataset = require('./data/items')
 
 http.globalAgent.maxSockets = Infinity;
@@ -15,11 +14,25 @@ app.use(express.json())
 app.use(cors())
 app.use(compression())
 
+require('./../swagger-setup')(app);
+
+/**
+ * @swagger
+ * /imoveis:
+ *  get:
+ *    tags: [API] 
+ *    description: Ao fazer uma requisição GET para a rota '/imoveis' irá retornar um JSON com todos os imóveis cadastrados
+ *    responses:
+ *      '200': 
+ *        description: Retorna um objeto JSON com os imóveis cadastrados
+
+ */
 app.get('/imoveis', (_, res) => {
   res.setHeader('Cache-Control', 'max-age=86400');
 
   res.json(dataset)
 })
+
 
 app.get('*', (_, res) => {
   res.status(404).end()
---
+++ swagger-setup.js
@@ -0,0 +1,22 @@
+const swaggerOptions = {
+  swaggerDefinition: {
+  info: {
+    title: "Desafio estágio AM Fernandes 2020",
+    description: "Essa API foi desenvolvida para o desafio da vaga de estágio em desenvolvimento de Software, para mais detalhes acesse: https://github.com/am-fernandes/api-estagio"
+  },
+  servers: ["http://localhost:8080"],
+  docExpansion: "full"
+  },
+  apis: ["./src/app.js"]
+}
+
+const opt = {
+  customCSS: '.topbar-wrapper img {content:url("https://amfernandes.com.br/logos/logo.png");'
+}
+
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const setup = app => app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerJsDoc(swaggerOptions, opt)));
+
+module.exports = setup;
