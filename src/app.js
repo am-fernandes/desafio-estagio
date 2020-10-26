9e7ef4ebcab8dbbee0e1de81538214712beaf17a
Matheus <matheusalexandre.sena99@gmail.com>
2020-10-25 23:33:56 -0300

    Enviando dados do dataset via HTTP

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const compression = require('compression')
 const http = require('http');
 const https = require('https');
+const fs = require('fs')
 
 http.globalAgent.maxSockets = Infinity;
 https.globalAgent.maxSockets = Infinity;
@@ -13,14 +14,14 @@ app.use(express.json())
 app.use(cors())
 app.use(compression())
 
+const dataset = JSON.parse(fs.readFileSync(__dirname + '/data/dataset.json', {encoding: 'utf-8'}))
+
 app.get('/imoveis', (_, res) => {
   res.setHeader('Cache-Control', 'max-age=86400');
 
-  res.send({
-    text: "hello world"
-  })
+  res.json(dataset)
 })
 
-app.listen(8000, () => {
+app.listen(8080, () => {
   process.stdout.write('listen on http://0.0.0.0:8080/imoveis\n')
 })
